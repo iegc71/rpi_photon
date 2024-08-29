44f1a7d7d57f6b2fcb9c3a484a49748a72887781
Ivan Garcia Cordero <iegc71@gmail.com>
2024-08-29 13:19:39 -0500

    Delete rpi_photon directory

--- rpi_photon/includes/snippets/js/off-mostrar-alerta.js
+++
@@ -1,1 +0,0 @@
-alert('Snippet desde js');
--- rpi_photon/includes/snippets/php/desactivar-generacion-variaciones.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Evita la creación de nuevas variaciones de imágenes
-function desactivar_generacion_variaciones( $sizes ) {
-    return [];
-}
-add_filter( 'intermediate_image_sizes_advanced', 'desactivar_generacion_variaciones' );
